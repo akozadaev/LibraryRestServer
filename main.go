607e3a7139fbb583bea716d905d326824680e98e
Alexey Kozadaev <alexey.kozadaev@gmail.com>
2023-10-13 17:48:16 +0300

    добавляет загрузку файла

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -77,6 +80,29 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+	file, handler, err := r.FormFile("file")
+	fileName := r.FormValue("file_name")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile("./files/" + handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, _ = io.WriteString(w, "File "+fileName+" Uploaded successfully")
+	_, _ = io.Copy(f, file)
+}
+
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Welcome home!")
+}
+
 func main() {
 	r := mux.NewRouter()
 	books = append(books, Book{ID: "1", Title: "Денискины рассказы", Author: &Author{Firstname: "Виктор", Lastname: "Драгунский"}})
@@ -87,7 +113,9 @@ func main() {
 	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 	fs := http.FileServer(http.Dir("./files"))
-	r.PathPrefix("/files").Handler(http.StripPrefix("/files", fs))
+	r.PathPrefix("/files/").Handler(http.StripPrefix("/files/", fs))
 
+	r.HandleFunc("/", homeLink)
+	r.HandleFunc("/file", UploadFile).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
